9b2ff825062bf5e2f1f58d18d17b160f6c765f72
William Ferreira <will.engecomp@gmail.com>
2021-04-06 23:34:37 -0300

    Primeiro commit

---
+++ src/main/java/william/apiwill/Controller/UsuarioController.java
@@ -0,0 +1,56 @@
+package william.apiwill.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import william.apiwill.Model.UsuarioModel;
+import william.apiwill.Repository.UsuarioRepository;
+
+import java.util.List;
+
+@RestController
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @GetMapping(path="/api/usuario/{id}")
+    public ResponseEntity consultar(@PathVariable("id") Integer id){
+        return repository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping(path="/api/usuario/salvar")
+    public UsuarioModel salvar(@RequestBody UsuarioModel usuario){
+
+        return repository.save(usuario);
+    }
+
+    @PutMapping(path="/api/usuario/{id}")
+    public ResponseEntity update(@PathVariable("id") Integer id,
+                                 @RequestBody UsuarioModel usuario) {
+        return repository.findById(id)
+                .map(record -> {
+                    record.setName(usuario.getName());
+                    record.setLogin(usuario.getLogin());
+                    record.setPass(usuario.getPass());
+                    UsuarioModel updated = repository.save(record);
+                    return ResponseEntity.ok().body(updated);
+                }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping(path="/api/usuario")
+    public List findAll(){
+        return (List) repository.findAll();
+    }
+
+    @DeleteMapping(path ={"/api/usuario/{id}"})
+    public ResponseEntity <?> delete(@PathVariable Integer id) {
+        return repository.findById(id)
+                .map(record -> {
+                    repository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
+    }
+}
---
+++ src/main/java/william/apiwill/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package william.apiwill.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csfr().disable();
+    }
+
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("william").password("will1991").roles("USER")
+//                .and()
+//                .withUser("admin").password("will199").roles("USER", "ADMIN");
+//    }
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("william").password(encoder.encode("1234")).roles("USER")
+                .and()
+                .withUser("admin").password(encoder.encode("1234")).roles("USER", "ADMIN");
+    }
+
+
+}
